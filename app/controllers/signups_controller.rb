e325fac83d2db64b65026f8918a8f5160cc170b5
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-08-16 09:52:15 -0600

    completed practice challenge" ; "

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,7 +2,7 @@ class SignupsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def create
-        signup = Signup.create!(signup_params)
+        signup = Signup.create(signup_params)
         render json: signup, status: :created
     end
 
